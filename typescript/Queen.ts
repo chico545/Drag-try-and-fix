e0be9bb757d2e9a909cdce9744884144e4fee579
chico545 <88053974+chico545@users.noreply.github.com>
2021-12-04 22:36:27 -0500

    Update Queen.ts

--- typescript/Queen.ts
+++ typescript/Queen.ts
@@ -482,23 +482,6 @@ let veracruz = new Queen("Veracruz", 9, 9, 9, 10, 12, 12, 7, "Veracruz");
 
 let lmd = [aurora, cpher, elektravandergeld, georgiana, irisxc, lacarrera, leexafox, lupita, morralisa, papercut, rebelmork, sirena, tiresias, veracruz];
 
-//DRAGULA
-let abhora = new Queen("Abhora", 7, 7, 8, 11, 9, 10, 8, "Abhora");
-let astrud = new Queen("Astrud Aurelia, 5, 5, 8, 12, 9, 9, 6, "Astrud");
-let biqtch = new Queen("Biqtch Puddin", 9, 10, 5, 4, 9, 4, 8, "Biqtch");
-let dahli = new Queen("Dahli", 9, 9, 9, 9, 9, 9, 9, "Dahli");
-let dollyablack = new Queen("Dollya Black", 6, 7, 9, 11, 10, 11, 10, "Dollyablack");
-let evahdestruction = new Queen("Evah Destruction, 7, 7, 8, 9, 9, 6, 6, "Evahdestruction");
-let hoso = new Queen("Hoso Terra Toma, 7, 8, 4, 12, 5, 13, 5, "Hoso Terra Toma");
-let jamesm = new Queen("James Majesty", 7, 7, 8, 11, 8, 11, 10, "Jamesm");
-let landoncider = new Queen("Landon Cider", 8, 8, 8, 11, 11, 9, 9, "Landoncider");
-let priscillachambers = new Queen("Priscilla Chambers", 9, 9, 8, 6, 8, 6, 9, "priscillachambers");
-let saint = new Queen("Saint, 7, 6, 9, 9, 6, 12, 3, "Saint");
-let sigourneybeaver = new Queen("Sigourney Beaver, 10, 8, 10, 9, 8, 10, 9, "SigourneyBeaver");
-let victoriae = new Queen("Victoria Elizabeth Black", 8, 6, 9, 14, 8, 15, 9, "Victoriae");
-
-let dragula = [abhora, astrud, biqtch, dahli, dollyablack, evahdestruction, hoso, jamesm, landoncider, priscillachambers, saint, sigourneybeaver, victoriae];
-
 //SPECIAL
 
 let pangina = new Queen("Pangina Heals", 10, 10, 9, 12, 9, 13, 11);
