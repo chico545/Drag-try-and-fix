d2f784d6d740fcd15f5ed461022dafb4675526cd
chico545 <88053974+chico545@users.noreply.github.com>
2021-12-04 22:35:32 -0500

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -2764,21 +2764,6 @@ var sirena = new Queen("Sirena", 8, 8, 9, 11, 7, 11, 9, "Sirena");
 var tiresias = new Queen("Tiresias", 7, 6, 7, 6, 6, 8, 9, "Tiresias");
 var veracruz = new Queen("Veracruz", 9, 9, 9, 10, 12, 12, 7, "Veracruz");
 var lmd = [aurora, cpher, elektravandergeld, georgiana, irisxc, lacarrera, leexafox, lupita, morralisa, papercut, rebelmork, sirena, tiresias, veracruz];
-//DRAGULA
-var abhora = new Queen("Abhora", 7, 7, 8, 11, 9, 10, 8, "Abhora");
-var astrud = new Queen("Astrud Aurelia, 5, 5, 8, 12, 9, 9, 6, "Astrud");
-var biqtch = new Queen("Biqtch Puddin", 9, 10, 5, 4, 9, 4, 8, "Biqtch");
-var dahli = new Queen("Dahli", 9, 9, 9, 9, 9, 9, 9, "Dahli");
-var dollyablack = new Queen("Dollya Black", 6, 7, 9, 11, 10, 11, 10, "Dollyablack");
-var evahdestruction = new Queen("Evah Destruction, 7, 7, 8, 9, 9, 6, 6, "Evahdestruction");
-var hoso = new Queen("Hoso Terra Toma, 7, 8, 4, 12, 5, 13, 5, "Hoso Terra Toma");
-var jamesm = new Queen("James Majesty", 7, 7, 8, 11, 8, 11, 10, "Jamesm");
-var landoncider = new Queen("Landon Cider", 8, 8, 8, 11, 11, 9, 9, "Landoncider");
-var priscillachambers = new Queen("Priscilla Chambers", 9, 9, 8, 6, 8, 6, 9, "priscillachambers");
-var saint = new Queen("Saint, 7, 6, 9, 9, 6, 12, 3, "Saint");
-var sigourneybeaver = new Queen("Sigourney Beaver, 10, 8, 10, 9, 8, 10, 9, "SigourneyBeaver");
-var victoriae = new Queen("Victoria Elizabeth Black", 8, 6, 9, 14, 8, 15, 9, "Victoriae");
-var dragula = [abhora, astrud, biqtch, dahli, dollyablack, evahdestruction, hoso, jamesm, landoncider, priscillachambers, saint, sigourneybeaver, victoriae];
 //SPECIAL
 var pangina = new Queen("Pangina Heals", 10, 10, 9, 12, 9, 13, 11);
 var international_as = [baga, blu, cheryl, janey, jimbo, jujubee, lemon, monique, pangina];
